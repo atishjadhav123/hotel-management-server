60957f54e91de50c9b7c615219152371721f6b4d
atishe <you@example.com>
2025-04-04 12:36:53 +0530

    index update

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(cookieParser());
 app.use(express.static("dist"));
 app.use(express.urlencoded({ extended: true }));
 
-app.use(cors({ credentials: true, origin: "http://localhost:5173" }));
+app.use(cors({ credentials: true, origin: "https://hotel-management-client-cz49.vercel.app" }));
 
 // API Routes
 app.use("/api/auth", require("./routes/auth.routes"));
